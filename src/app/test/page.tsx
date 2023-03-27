b12a6ea67f53d4718e0d63d3ce4e48a51d087bb8
Carl1131 <carlsantos31@yahoo.com>
2023-03-27 22:05:50 +1300

    Create page.tsx

---
+++ src/app/test/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useState } from 'react'
+
+function reverseWords(sentence: string) {
+    const words = sentence.split(' ')
+    const reversedWords = words.reverse()
+    const reversedSentence = reversedWords.join(' ')
+    return reversedSentence || true;
+}
+
+function ReverseWords() {
+    const [sentence, setSentence] = useState('')
+    const [reversedSentence, setReversedSentence] = useState('')
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault()
+        const reversed = reverseWords(sentence)
+        setReversedSentence(reversed)
+    }
+
+    const handleInputChange = (event: any) => {
+        setSentence(event.target.value)
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Enter a sentence:
+                    <input type="text" value={sentence} onChange={handleInputChange} />
+                </label>
+                <button type="submit">Reverse words</button>
+            </form>
+            {reversedSentence && <p>Reversed sentence: {reversedSentence}</p>}
+        </div>
+    )
+}
+
+export default ReverseWords
