f4c9652bbbc87a5d8e910dd27b4601582e3a335d
Carl1131 <carlsantos31@yahoo.com>
2023-04-25 17:29:32 +1200

    Settings tab is up!! :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+"use client";
 import { useState } from "react";
 
 export default function Home() {
+	const [popupVisible, setPopupVisible] = useState(false);
+
+	function togglePopup() {
+		setPopupVisible(!popupVisible);
+	}
+
 	return (
 		<main>
 			<div className="h-full flex-grow bg-gray-100">
@@ -61,9 +68,42 @@ export default function Home() {
 							quam libero id odio. Sed volutpat velit nec nulla dictum
 							convallis.
 						</p>
+
+					</div>
+					<div className="bg-white rounded-lg shadow-lg p-6">
+						<div onClick={togglePopup} id="settings-icon" className="bg-white rounded-lg shadow p-4">
+							<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="black">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"></path>
+							</svg>
+						</div>
 					</div>
 				</div>
 			</div>
+			<div>
+
+
+				{popupVisible && (
+					<div className="absolute top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex items-center justify-center z-50">
+					<div className="bg-white p-8 rounded shadow-md">
+					  <h2 className="text-xl text-black font-bold mb-4">Settings</h2>
+					  <label htmlFor="input" className="block text-gray-700 font-bold mb-2">Variable 1</label>
+					  <input id="input" type="text" className="border border-gray-300 p-2 rounded-md"></input>
+
+					  <button className="block mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+						Save Settings
+					  </button>
+					  <button className="mt-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+							  onClick={togglePopup}>
+						Close
+					  </button>
+					</div>
+				  </div>
+				  
+				)}
+			</div>
+
 		</main>
+
+
 	);
 }
