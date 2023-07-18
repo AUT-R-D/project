ff6513f13b134b0623606a961e9d013ec8177a00
TheMysterys <the-mystery@outlook.com>
2023-07-18 21:49:13 +1200

    openAI base code

---
+++ src/lib/openai.ts
@@ -0,0 +1,27 @@
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
+
+if (!process.env.OPENAI_API_KEY) {
+	throw new Error("Invalid/Missing environment variable: 'OPENAI_API_KEY'");
+}
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const config = new Configuration({
+	apiKey: OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+export async function sendMessage(message: string, messages: ChatCompletionRequestMessage[]) {
+    messages.push({role: "user", content: message})
+	const completion = await openai.createChatCompletion({
+		model: "gpt-3.5-turbo",
+		messages: messages,
+	});
+
+    const responseMessage = completion.data.choices[0].message;
+
+    return responseMessage;
+}
+
+
