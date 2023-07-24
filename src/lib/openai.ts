d8544ad6638a4dc1dcfe8f22dbca85560237adad
Carl1131 <carlsantos31@yahoo.com>
2023-07-24 19:44:43 +1200

    Added prompt
    Added the chatgpt prompt that will be our basis for prompt design and incorporating user input.

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -13,11 +13,38 @@ const config = new Configuration({
 
 const openai = new OpenAIApi(config);
 
+// Goal
+// 1 - To use user-set variables within the prompt below (Type of company, slang and variables).
+// 2 - To have all key information from user be saved into our mongo DB.
+
+const prompt = "Pretend you are a chatbot for an insurance company. \
+You must speak with slang level (out of 10): 3 \
+\
+The variables below and the required information from the user are: \
+1) policy number: requires full name, type of insurance, and date of insurance. \
+2) Customer address: requires full name, and date of sign up with this company. \
+\
+Here's an example of a conversation: \
+Chatbot: how can I help you today? \
+User: I want to know my policy number. \
+Chatbot: Sure! What is your full name? \
+User: Mike Ross \
+Chatbot: Thanks Mike! What is your type of insurance? \
+User: comprehensive home insurance. \
+Chatbot: And when did you sign up for this insurance cover? \
+User: 25/4/21 \
+Chatbot: OK! Your policy number is 34720. Is there anything else you want me to help you with today? \
+User: Its ok. \
+Chatbot: Okay! Ill be waiting! \
+\
+Conversation begin";
+
 export async function sendMessage(
 	message: string,
 	messages: ChatCompletionRequestMessage[]
 ) {
 	try {
+		//messages.push({ role: "user", content: prompt + message });
 		messages.push({ role: "user", content: message });
 
 		const completion = await openai.createChatCompletion({
