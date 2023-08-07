7bf8d62a359d67a21a0b633462f5d0405406e50f
TheMysterys <the-mystery@outlook.com>
2023-08-07 22:41:29 +1200

    Added prompt

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -13,7 +13,10 @@ export async function POST(request: NextRequest) {
 	const messages = res.messages || [];
 
 	if (!message || message.length < 1) {
-		return NextResponse.json({ error: "Message not found" }, { status: 400 });
+		return NextResponse.json(
+			{ error: "Message not found" },
+			{ status: 400 }
+		);
 	}
 
 	const conversation_id = res.conversation_id;
@@ -27,13 +30,41 @@ export async function POST(request: NextRequest) {
 
 	const client = await clientPromise;
 	const db = client.db("info-flow");
-	const collection = db.collection("conversations");
+	const conversations = db.collection("conversations");
+	const settings = (await db.collection("settings").findOne({})) as any;
+
+	const prompt = [
+		`Pretend you are a chatbot for an ${settings.scenario} company. `,
+		`You must speak with slang level (out of 10): ${settings.slag || 5} `,
+		``,
+		`Here's an example of a conversation: `,
+		`Chatbot: how can I help you today? `,
+		`User: I want to know my policy number. `,
+		`Chatbot: Sure! What is your full name? `,
+		`User: Mike Ross `,
+		`Chatbot: Thanks Mike! What is your type of insurance? `,
+		`User: comprehensive home insurance. `,
+		`Chatbot: And when did you sign up for this insurance cover? `,
+		`User: 25/4/21 `,
+		`Chatbot: OK! Your policy number is 34720. Is there anything else you want me to help you with today? `,
+		`User: Its ok. `,
+		`Chatbot: Okay! Ill be waiting! `,
+		``,
+		`The variables below and the required information from the user are:`,
+	];
+
+	for (const variable of Object.values(settings.variables) as {
+		name: string;
+		value: string;
+	}[]) {
+		prompt.push(`Variable '${variable.name.trim()}': '${variable.value.trim()}'`);
+	}
 
 	try {
 		if (messages.length == 0) {
 			messages.push({
 				role: "system",
-				content: "You are an unhelpful assistant.",
+				content: prompt.join("\n"),
 			});
 		}
 
@@ -46,7 +77,7 @@ export async function POST(request: NextRequest) {
 		// Add the response to the messages array
 		messages.push({ role: "assistant", content: response });
 
-		await collection.updateOne(
+		await conversations.updateOne(
 			{ conversation_id: conversation_id },
 			{ $set: { messages } },
 			{ upsert: true }
