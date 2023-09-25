88568afc6c0b9ecff3ab33b90b04f7a81f156c6a
TheMysterys <the-mystery@outlook.com>
2023-09-26 10:47:32 +1300

    Find JSON from message

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -2,6 +2,28 @@ import clientPromise from "@/lib/mongodb";
 import { sendMessage } from "@/lib/openai";
 import { NextRequest, NextResponse } from "next/server";
 
+function extractJSON(str: string) {
+	let firstOpen = 0;
+	let firstClose;
+	let candidate;
+	firstOpen = str.indexOf("{", firstOpen + 1);
+	do {
+		firstClose = str.lastIndexOf("}");
+		if (firstClose <= firstOpen) {
+			return null;
+		}
+		do {
+			candidate = str.substring(firstOpen, firstClose + 1);
+			try {
+				let res = JSON.parse(candidate);
+				return res
+			} catch (ignored) {}
+			firstClose = str.substr(0, firstClose).lastIndexOf("}");
+		} while (firstClose > firstOpen);
+		firstOpen = str.indexOf("{", firstOpen + 1);
+	} while (firstOpen != -1);
+}
+
 export async function POST(request: NextRequest) {
 	const res = await request.json();
 
@@ -39,13 +61,40 @@ export async function POST(request: NextRequest) {
 	const client = await clientPromise;
 	const db = client.db("info-flow");
 	const conversations = db.collection("conversations");
+	const userData = db.collection("user-data");
 
 	try {
 		// Commented out for now since no access to OpenAI API
 		const rawResponse = await sendMessage(message, messages, model);
 
-		const response = rawResponse!.content;
-		//const response = `You're message was: ${message}`
+		if (!rawResponse) {
+			return NextResponse.json(
+				{ error: "No response from Chatbot" },
+				{ status: 500 }
+			);
+		}
+
+		const response = rawResponse.content;
+
+		if (!response) {
+			return NextResponse.json(
+				{ error: "No response from Chatbot" },
+				{ status: 500 }
+			);
+		}
+
+		// Extract the JSON from the response
+		const json = extractJSON(response);
+
+		if (json) {
+			console.log("JSON Found in response")
+
+			await userData.updateOne(
+				{ conversation_id: conversation_id },
+				{ $set: { data: json } },
+				{ upsert: true }
+			);
+		}
 
 		// Add the response to the messages array
 		messages.push({ role: "assistant", content: response });
