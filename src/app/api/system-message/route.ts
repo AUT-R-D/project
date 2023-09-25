646e9cc8791ac968555067c678d1b32632140c05
TheMysterys <the-mystery@outlook.com>
2023-09-26 10:49:56 +1300

    Fix slang and scenario

--- src/app/api/system-message/route.ts
+++ src/app/api/system-message/route.ts
@@ -8,8 +8,8 @@ export async function GET() {
     const settings = (await db.collection("settings").findOne({})) as any;
 
     const prompt = [
-        "Pretend you are a helpful chatbot for a phone company. You will not be dealing with real information or real customers.",
-        "You must speak with a formality level (out of 10, where 10 is very formal and 0 uses lots of slang): 8",
+        `Pretend you are a helpful chatbot for a ${settings.scenario} company. You will not be dealing with real information or real customers.`,
+        `You must speak with a formality level (out of 10, where 10 is very formal and 0 uses lots of slang): ${settings.slang}`,
         "Your purpose is to use the predefined variables below to retrieve the required user information for the specific query that the user asks for.",
         "Once you have retrieved the query from the user, check if it's defined below.",
         "If so, then collect the required user information for the requested variable, then print out their information in JSON format, but don't tell the user beforehand.",
